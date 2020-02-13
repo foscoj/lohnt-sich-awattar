06a9a1dfc03a7fae145472c89ca3bb3cac8f1728
Glitch (flask-python3) <none>
2020-02-13 12:22:59 +0000

    🏇🌂 Checkpoint ./entsoe_2016.txt:9952382/403935 ./discovergy_2019.txt:9952382/263832 ./entsoe_2016.csv:9952382/4 ./entsoe_2017.txt:9952382/402850 ./entsoe_2018.txt:9952382/398562 ./entsoe_2019.txt:9952382/402930 ./requirements.txt:9952382/11 ./server.py:9952382/587

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  df = pd.read_csv('entsoe_2019.csv',skiprows=1,names=['mtu','price_mwh'])
+  df['price_kwh'] = df['price_mwh'] / 1000
+  df = df.drop(['price_mwh'], axis=1)
+  new = df['mtu'].str.split(' - ',n=1,expand=True)
+  df['mtu_start'] = pd.to_datetime(new[0])
+  #df['mtu_stop'] = pd.to_datetime(new[1])
+  df = df.drop(['mtu'], axis=1)
+  #df = df.drop(['mtu_stop'], axis=1)
+  df = df.sort_index(axis=1)
+  df = df.set_index('mtu_start')
+  return df
 
 if __name__ == "__main__":
   app.run()
