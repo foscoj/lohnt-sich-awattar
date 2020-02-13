b1ff29a4af8652be1763c5ea2a60f0385b5fbee9
Glitch (flask-python3) <none>
2020-02-13 12:35:01 +0000

    ✊🍣 Checkpoint ./server.py:9952382/569 ./requirements.txt:9952382/5

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 from flask import Flask
 import pandas as pd
+import glob
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  df = pd.read_csv('entsoe_2019.csv',skiprows=1,names=['mtu','price_mwh'])
+  extension = 'csv'
+  csvs = glob.glob('entsoe*.{}'.format(extension))
+  print(csvs)
+  
+  li = []
+
+  for csv in csvs:
+      df = pd.read_csv(csv, ,skiprows=1,names=['mtu','price_mwh'])
+      li.append(df)
+
+  df = pd.concat(li, axis=0, ignore_index=True)
+  
+  
+  #df = pd.read_csv('entsoe_2019.csv',skiprows=1,names=['mtu','price_mwh'])
   df['price_kwh'] = df['price_mwh'] / 1000
   df = df.drop(['price_mwh'], axis=1)
   new = df['mtu'].str.split(' - ',n=1,expand=True)
@@ -14,7 +28,7 @@ def hello():
   #df = df.drop(['mtu_stop'], axis=1)
   df = df.sort_index(axis=1)
   df = df.set_index('mtu_start')
-  return df
+  return df.to_html()
 
 if __name__ == "__main__":
   app.run()
