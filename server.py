6364c8436f2591bac3e8b3185f7d4b3a8e90dc3e
Glitch (flask-python3) <none>
2020-02-13 14:27:14 +0000

    ✍️🏓 Checkpoint ./server.py:9952382/924 ./requirements.txt:9952382/47

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
 from flask import Flask
 import pandas as pd
 import glob
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+  #read csv data (should only be read of db does not exist)
   extension = 'csv'
   all_files = glob.glob('entsoe*.{}'.format(extension))
   print(all_files)
   df = pd.concat((pd.read_csv(f,index_col=None,skiprows=1,names=['mtu','price_mwh'],na_values='n/e') for f in all_files),ignore_index=True)
-  #df = pd.read_csv('entsoe_2019.csv',skiprows=1,names=['mtu','price_mwh'])
+  # ---------------- #  
+  #create dataframe with all necessary inputs from entsoe
   df['price_kwh'] = df['price_mwh'] / 1000
   df = df.drop(['price_mwh'], axis=1)
   new = df['mtu'].str.split(' - ',n=1,expand=True)
   df['mtu_start'] = pd.to_datetime(new[0])
-  #df['mtu_stop'] = pd.to_datetime(new[1])
+  df.rename(columns={"": "a", "B": "c"})
   df = df.drop(['mtu'], axis=1)
-  #df = df.drop(['mtu_stop'], axis=1)
-  #df = df.sort_index(axis=1)
   df = df.set_index('mtu_start')
+  # ---------------- #
+  #write data to sqlite database
+  conn = sqlite3.connect('.data/entsoe.db')
+  c = conn.cursor()
+  c.execute('CREATE TABLE PRICES (timestamp, price_kwh)')
+  conn.commit()
+  
+  df.to_sql('PRICES', conn, if_exists='replace', index = False)
+  
   return df.to_html()
 
 if __name__ == "__main__":
